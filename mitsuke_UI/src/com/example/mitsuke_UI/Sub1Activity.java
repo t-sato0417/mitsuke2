09347c11dfb3d14dafe679c4599b64ee8efe8812
takumi_sato <tarutaru0417@gmail.com>
2014-09-18 16:18:25 +0900

    Revert "見附２　UI部分　完成率４０％（エラー部分あり）"
    This reverts commit 63ee7349008e542d405acb5548d2ec9a04edd351.

--- mitsuke_UI/src/com/example/mitsuke_UI/Sub1Activity.java
+++ mitsuke_UI/src/com/example/mitsuke_UI/Sub1Activity.java
@@ -1,111 +1,44 @@
 package com.example.mitsuke_UI;
 
-import com.example.mitsuke_UI.R;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.navdrawer.SimpleSideDrawer;
-//import com.navdrawer.SimpleSideDrawer;
-
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.navdrawer.SimpleSideDrawer;
+import android.view.Menu;
+import android.view.View.OnClickListener;
 
-class mapcontrol{
-	GoogleMap map;
-	LatLng deflatlng=new LatLng(43.0675,141.350784);
-	void setMap(GoogleMap map){
-		this.map = map;
-	}
-	void SaveDate(){
-		 deflatlng =new LatLng(
-				 map.getMyLocation().getLatitude(),
-				 map.getMyLocation().getLongitude());
-	}
-	void CameraUpdate(){
-		CameraUpdate cu = 
-			CameraUpdateFactory.newLatLngZoom(
-					deflatlng, 15);
-		map.moveCamera(cu);
-	}
-}
+public class Sub1Activity extends Activity {
 
-public class Sub1Activity extends FragmentActivity{
-	
 	public SimpleSideDrawer mSlidingMenu;
-	public static final String TAG = "TEST";
-	static GoogleMap map;
-	static int initialized =0;
-	static mapcontrol mapdate=new mapcontrol();
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		System.out.println("FRAGMENT PRINT POINT");
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sub1);
-					
-		Button btn4 = (Button) findViewById(R.id.btn4);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+ 
+        Button btn4 = (Button) findViewById(R.id.btn4);
         Button btn5 = (Button) findViewById(R.id.btn5);
-        
+ 
         btn4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(Sub1Activity.this, 
+                Intent intent = new Intent(Sub1Activity.this,
                     MainActivity.class );
                 startActivity(intent);
-                finish(); // ﾆ但ﾆ誰ﾆ弾ﾆ達ﾆ池ﾆ弾ﾆ達ﾂ終窶板ｹ
+                finish(); // アクティビティ終了
             }
         });
+       
         mSlidingMenu = new SimpleSideDrawer(this);
         mSlidingMenu.setLeftBehindContentView(R.layout.side_menu);
         btn5.setOnClickListener(new OnClickListener() {
+        	
             @Override 
             public void onClick(View v) {
             	mSlidingMenu.toggleLeftDrawer();
             }
         });
-        
-		map = ((SupportMapFragment)
-				getSupportFragmentManager().findFragmentById(R.id.map))
-				.getMap();
-		MapsInitializer.initialize(this);
-		mapdate.setMap(map);
-		
-		mapdate.CameraUpdate();
-		
-	}
-	protected void onDestroy(){
-		mapdate.SaveDate();
-		super.onDestroy();
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return false;
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		/*int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-		return true;*/
-	}
-	
 }
--- mitsuke_UI/src/com/example/mitsuke_UI/Sub2Activity.java
+++ mitsuke_UI/src/com/example/mitsuke_UI/Sub2Activity.java
@@ -1,73 +1,41 @@
 package com.example.mitsuke_UI;
 
 //import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.support.v4.app.FragmentActivity;
 
 
-public class Sub2Activity extends FragmentActivity {
+public class Sub2Activity extends Activity {
 
 	 @Override
-	    public void onCreate(Bundle savedInstanceState) {
+	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_sub2);
-	        Button btn8 = (Button) findViewById(R.id.btn8);
-	        
-	        btn8.setOnClickListener(new View.OnClickListener() {
-	            @Override
+	        setTitle("画面3");
+	 
+	        Button btn6 = (Button) findViewById(R.id.btn6);
+	        Button btn7 = (Button) findViewById(R.id.btn7);
+	 
+	        btn6.setOnClickListener(new View.OnClickListener() {
 	            public void onClick(View v) {
-	                Intent intent = new Intent(Sub2Activity.this,
-	                    Sub3Activity.class );
+	                Intent intent = new Intent(Sub2Activity.this, 
+	                    MainActivity.class );
 	                startActivity(intent);
+	                finish(); // アクティビティ終了
 	            }
 	        });
-	  
-	        // ArrayAdapter を作成  
-	        String items[] = {"カテゴリを選んでください","items1","items2"};
-	        ArrayAdapter<String> adapter
-	                = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-	        //または
-	        //ArrayAdapter<String> adapter
-	        //        = new ArrayAdapter<string>(this, android.R.layout.simple_spinner_item）
-	        //adapter.add("item1");
-	        //adapter.add("item2");
-	        //adapter.add("item3");
-	        
-	        // ドロップダウンリストのレイアウトを設定   
-	        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);  
-	         
-	        //spinnerにAdapterを設定
-	        Spinner spinner  = (Spinner)this.findViewById(R.id.Spinner01);
-	        spinner.setAdapter(adapter);
-	        // Spinner に表示させるプロンプトを設定   
-	        spinner.setPromptId(R.string.SpinnerPrompt );
-	  
-	        // スピナーのアイテムが選択された時に呼び出されるコールバックリスナーを登録します
-	        /*spinner.setOnItemSelectedListener(new OnItemSelectedListener(){
-	            //Spinnerのドロップダウンアイテムが選択された時
-	            public void onItemSelected(AdapterView parent, View viw, int arg2, long arg3) {
-	                Spinner spinner = (Spinner)parent;
-	                String item = (String)spinner.getSelectedItem();
-	                Toast.makeText(Sub2Activity.this, item, Toast.LENGTH_LONG).show();
+	 
+	        btn7.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	                Intent intent = new Intent(Sub2Activity.this,
+	                    Sub1Activity.class );
+	                startActivity(intent);
+	                finish(); // アクティビティ終了
 	            }
-	            //Spinnerのドロップダウンアイテムが選択されなかった時
-	            public void onNothingSelected(AdapterView parent) {
-	            }});*/
+	        });
 	    }
-	 @Override
-	    protected void onStart () {
-	    super.onStart();
-	    // 初期選択状態の設定
-	        ((Spinner)findViewById(R.id.Spinner01)).setSelection(0);
-	    }
-
 
 }
